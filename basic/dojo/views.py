d9d7d14364e693237fa1b54becdc820d545dbf1a
Junhong-Kim <jhk1005jhk@gmail.com>
2018-01-06 04:41:07 +0900

    Added, dojo.views.hello

--- basic/dojo/views.py
+++ basic/dojo/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def mysum(request, numbers):
     result = sum(map(lambda s: int(s or 0), numbers.split("/")))
     return HttpResponse(result)
+
+
+def hello(request, name, age):
+    return HttpResponse("Hello, {} ({})".format(name, age))
