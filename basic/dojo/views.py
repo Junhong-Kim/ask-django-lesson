97f9a106b261e2f3da86810532f18725f2f6bd65
Junhong-Kim <jhk1005jhk@gmail.com>
2018-01-06 13:23:53 +0900

    Added, function based view

--- basic/dojo/views.py
+++ basic/dojo/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+import os
+
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 
@@ -9,3 +12,38 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse("Hello, {} ({})".format(name, age))
+
+
+# HttpResponse
+def post_list1(request):
+    name = 'Kim'
+    return HttpResponse('''
+        <h1>Ask Django</h1>
+        <p>{name}</p>
+        <p>Life is too short, You need Python<p>
+    '''.format(name=name))
+
+
+# Template
+def post_list2(request):
+    name = 'Kim'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+# JsonResponse
+def post_list3(request):
+    return JsonResponse({
+        'message': 'Hello, Python & Django',
+        'items': ['Python', 'Django'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+
+# FileDownload
+def excel_download(request):
+    # os.path.join(settings.BASE_DIR, 'work.xlsx')
+    filepath = '/Users/INMA/Downloads/work.xlsx'
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
