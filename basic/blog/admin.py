6e747265a2cd4e062a2b875e27b32a5f59d9968c
Junhong-Kim <jhk1005jhk@gmail.com>
2018-01-08 14:37:18 +0900

    Modified, blog.models.Post admin

--- basic/blog/admin.py
+++ basic/blog/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+# 등록법 3
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+
+
+# 등록법 2
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'title', 'created_at', 'updated_at']
+#
+#
+# admin.site.register(Post, PostAdmin)
+
+
+# 등록법 1
+# admin.site.register(Post)
