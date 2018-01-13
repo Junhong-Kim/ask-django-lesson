a0a7bd43c2fb2c3c1a0b4ea7180310954c765138
Junhong-Kim <jhk1005jhk@gmail.com>
2018-01-14 01:00:06 +0900

    Added, greeting FBV

--- cbv/blog/views.py
+++ cbv/blog/views.py
@@ -1,20 +1,43 @@
 from django.http import HttpResponse
-from django.views import View
+# from django.views import View
 
 
-class GreetingView(View):
-    message = 'Good Day'
+# class GreetingView(View):
+#     message = 'Good Day'
+#
+#     def get(self, *args, **kwargs):
+#         return HttpResponse(self.message)
+#
+#
+# greeting = GreetingView.as_view()
+#
+#
+# class MorningGreetingView(GreetingView):
+#     message = 'Morning to ya'
+#
+#
+# morning_greeting = MorningGreetingView.as_view()
+# evening_greeting = MorningGreetingView.as_view(message='Evening to ya')
 
-    def get(self, *args, **kwargs):
-        return HttpResponse(self.message)
+
+# def greeting(request, message='Good Day'):
+#     return HttpResponse(message)
+#
+#
+# def morning_greeting(request):
+#     return greeting(request, 'Morning to ya')
+#
+#
+# def evening_greeting(request):
+#     return greeting(request, 'Evening to ya')
 
 
-greeting = GreetingView.as_view()
+def greeting_view(message):
+    def view_fn(request):
+        return HttpResponse(message)
+    return view_fn
 
 
-class MorningGreetingView(GreetingView):
-    message = 'Morning to ya'
-
-
-morning_greeting = MorningGreetingView.as_view()
-evening_greeting = MorningGreetingView.as_view(message='Evening to ya')
+greeting = greeting_view('Good Day')
+morning_greeting = greeting_view('Morning to ya')
+evening_greeting = greeting_view('Evening to ya')
